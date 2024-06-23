bbd773415045703bb7935790640af972486f493a
BananaPuppy <bananabaconpancakes07@gmail.com>
2024-06-22 18:14:34 -0700

    Init

---
+++ src/main/java/com/bananapuppy/vanillatweaker/VanillaTweakerClient.java
@@ -0,0 +1,10 @@
+package com.bananapuppy.vanillatweaker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaTweakerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/bananapuppy/vanillatweaker/VanillaTweakerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bananapuppy.vanillatweaker;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaTweakerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
